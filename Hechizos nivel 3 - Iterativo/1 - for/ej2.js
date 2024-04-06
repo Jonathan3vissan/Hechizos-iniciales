e7ac784d4cdf35d7382083fd1aabebd49532ff4a
Jonnthan3visan <joptrabajo@hotmail.com>
2024-04-06 19:01:33 -0300

    nivel 3

--- Hechizos nivel 3 - Iterativo/1 - for/ej2.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej2.js
@@ -2,7 +2,14 @@ const leer = require("prompt-sync")();
 
 /**
  * Ej 2
- * Los Dementores son criaturas oscuras que se alimentan de la felicidad de las personas, dejándolas con recuerdos dolorosos y desesperación. Para protegerse de los Dementores, los estudiantes de Hogwarts aprenden el hechizo Patronus, que repele a estas criaturas y los aleja con recuerdos felices. En este ejercicio, simularemos un encuentro con un Dementor en un mini juego donde un estudiante debe lanzar el hechizo Patronus correctamente para repeler al Dementor y proteger su felicidad. Dependiendo de la cantidad de vida del estudiante, se necesitará ingresar el hechizo Patronus varias veces para vencer al Dementor. Si el estudiante no ingresa el hechizo correctamente, perderá turnos y el Dementor consumirá su felicidad y recuerdos felices, acercándose más a la derrota del estudiante. En otro caso si el estudiante ingresa bien los hechizos y logra vencer al dementor entonces se mostrar un mensaje de victoria
+ * Los Dementores son criaturas oscuras que se alimentan de la felicidad de las personas, dejándolas con recuerdos dolorosos
+ *  y desesperación. Para protegerse de los Dementores, los estudiantes de Hogwarts aprenden el hechizo Patronus, 
+ * que repele a estas criaturas y los aleja con recuerdos felices. En este ejercicio, simularemos un encuentro con un
+ *  Dementor en un mini juego donde un estudiante debe lanzar el hechizo Patronus correctamente para repeler al Dementor y 
+ * proteger su felicidad. Dependiendo de la cantidad de vida del estudiante, se necesitará ingresar el hechizo Patronus 
+ * varias veces para vencer al Dementor. Si el estudiante no ingresa el hechizo correctamente, perderá turnos y el Dementor 
+ * consumirá su felicidad y recuerdos felices, acercándose más a la derrota del estudiante. En otro caso si el estudiante
+ *  ingresa bien los hechizos y logra vencer al dementor entonces se mostrar un mensaje de victoria
  * 
  * Vida maxima del dementor: 1000
  * Vida maxima del estudiante: 350
@@ -13,8 +20,43 @@ const leer = require("prompt-sync")();
 
 
 function main() {
-    
-}
+    const ATAQUE_MAGO = 267;
+    const DAÑO_TURNO_PERDIDO_A_MAGO = 75;
+    const PATRONUS = "Patronus";
+    let vida_estudiante = 350;
+    let vida_dementor = 1000;
+    let hechizo = "defoul";
+    console.log("inicamos la batalla");
 
+    for (let i = 5; i > 0; i--) {
+
+        if ((vida_dementor <= 0) || (vida_estudiante <= 0)) {
+            i = 0
+        } else {
+            console.log(" lanza hechizo");
+            hechizo = leer();
+
+
+        }
+        if ((PATRONUS == hechizo) && (vida_dementor > 0)) {
+            console.log("acertaste tu ataque");
+            vida_dementor = vida_dementor - ATAQUE_MAGO
+            console.log("vida del demontor:", vida_dementor);
+
+        } else if ((hechizo != PATRONUS) && (vida_estudiante > 0)) {
+            console.log("erraste tu ataque");
+            vida_estudiante = vida_estudiante - DAÑO_TURNO_PERDIDO_A_MAGO
+            console.log("vida del estudiante:", vida_estudiante);
+        }
+
+
+
+
+
+
+
+
+    }
+}
 
 main();
