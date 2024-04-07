9a09f501995fd3073600d2b076d5296d9fb98d8a
Jonnthan3visan <joptrabajo@hotmail.com>
2024-04-07 11:23:58 -0300

    actualizado

--- Hechizos nivel 3 - Iterativo/1 - for/ej3.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej3.js
@@ -29,110 +29,108 @@ const leer = require("prompt-sync")();
  * 
  */
 
-function main(){
+function main() {
 
-let sanguijuelas_reventadas = 0;
-let pulverizado_cuerno_bicornio = 0;
-let pelo_gato_negro = 0;
-let cola_serpiente = 0;
-let sentido_de_giro = "def";
+   let sanguijuelas_reventadas = 0;
+   let pulverizado_cuerno_bicornio = 0;
+   let pelo_gato_negro = 0;
+   let cola_serpiente = 0;
+   let sentido_de_giro = "def";
 
-console.log("Hoy prepararemos la pocion Multijugos");
-console.log("comenzemos por los ingrediente , solo le dare 3 intentos para decir la cantidad correcta de cada ingrediente");
-console.log("primer ingrediente: sanguijuelas reventadas, deben decirme la cantidad precisa tiene solo 3 intenos");
-console.log("recuerda que si no sabes la cantida exacta de algun igrediente no podras continuar dejando imcopleta esta tarea");
+   console.log("Hoy prepararemos la pocion Multijugos");
+   console.log("comenzemos por los ingrediente , solo le dare 3 intentos para decir la cantidad correcta de cada ingrediente");
+   console.log("primer ingrediente: sanguijuelas reventadas, deben decirme la cantidad precisa tiene solo 3 intenos");
+   console.log("recuerda que si no sabes la cantida exacta de algun igrediente no podras continuar dejando imcopleta esta tarea");
 
-for (let i = 0; i < 3; i++) {
-   console.log("cantidad exacta de sanguijuelas reventadas");
-   sanguijuelas_reventadas = leer();
-   if (sanguijuelas_reventadas != 3) {
-      console.log("cantiodad incorrecta");
-      console.log("intento N:", i + 1);
+   for (let i = 0; i < 3; i++) {
+      console.log("cantidad exacta de sanguijuelas reventadas");
+      sanguijuelas_reventadas = leer();
+      if (sanguijuelas_reventadas != 3) {
+         console.log("cantiodad incorrecta");
+         console.log("intento N:", i + 1);
 
-   } else {
-      console.log("continuemos");
-      i = 3;
-
-   }//fin del if
-
-
-}                                                 // fin del ford
-
-if (sanguijuelas_reventadas == 3) {
-
-
-   console.log("bien ahora vamos empezar a preparar la pocion ");
-   console.log("ahora agitamos la mezcla en el caldero durante 30 segundo");
-
-} else {
-   
-
-}                                       //fin del if           del bloque comnpleto del if                            
-
-if (sanguijuelas_reventadas == 3) {  //dar bola este
-
-   for (let j = 0; j < 3; j++) {
-
-      console.log("que cantidad de pulverizado de cuerno de bicornio? ");
-      pulverizado_cuerno_bicornio = leer();
-      if (pulverizado_cuerno_bicornio != 5) {
-         console.log("incorrecto.intento numero", j + 1);
       } else {
-         console.log("bien, repuesta correcta ");
-         j = 3;
+         console.log("continuemos");
+         i = 3;
+
       }
 
-   } //for fin
+
+   }
+
+   if (sanguijuelas_reventadas == 3) {
 
 
-} else { 
+      console.log("bien ahora vamos empezar a preparar la pocion ");
+      console.log("ahora agitamos la mezcla en el caldero durante 30 segundo");
 
-}                                                                 //fin de este bloque if
-if ((sanguijuelas_reventadas==3)&&(pulverizado_cuerno_bicornio==5)) {
-   
+   } else {
 
-   console.log("muy bienn, ahora greagamos al caldero pulverizado de cuerno de bicorino y lo revolvemos con cuchara de palo");
-   
-   for (let k = 0; k < 3; k++) {
-      console.log("ahora que cantidad de de pelo de gato negro?");
-      pelo_gato_negro=leer();
-      if (pelo_gato_negro!=1) {
-         console.log("error, intento numero:",k+1);
-         
-      }else {console.log("excelente,nos queda uno mas");
-        k=3;
-      }//fin del if
-            
 
-   }// fin del rof
-   
-}else {
+   }
 
-}//fin de este bloque if
-if ((sanguijuelas_reventadas==3)&&(pulverizado_cuerno_bicornio==5)&&(pelo_gato_negro==1)) {
+   if (sanguijuelas_reventadas == 3) {
 
-   console.log("ahora el ultimo ingrediente");
-   
-   for (let l = 0; l < 3; l++) {
-      console.log("que cantidad de cola de serpiente?");
-      cola_serpiente=leer();
-      if (cola_serpiente!=2) {
-         console.log("maaaal, intento numero:",l+1);
-      }else{ 
-         
-         console.log("muy bien, completaste con exito la pocion ");
-         l=3;
+      for (let j = 0; j < 3; j++) {
 
-      }//fin del if
+         console.log("que cantidad de pulverizado de cuerno de bicornio? ");
+         pulverizado_cuerno_bicornio = leer();
+         if (pulverizado_cuerno_bicornio != 5) {
+            console.log("incorrecto.intento numero", j + 1);
+         } else {
+            console.log("bien, repuesta correcta ");
+            j = 3;
+         }
 
-   }//fin del for
+      }
 
-}else{
-   console.log("volve mañana  fin");
 
-}//fin d este tercer bloque if
+   } else {
 
-// salida de la funcion
+   }
+   if ((sanguijuelas_reventadas == 3) && (pulverizado_cuerno_bicornio == 5)) {
 
+
+      console.log("muy bienn, ahora greagamos al caldero pulverizado de cuerno de bicorino y lo revolvemos con cuchara de palo");
+
+      for (let k = 0; k < 3; k++) {
+         console.log("ahora que cantidad de de pelo de gato negro?");
+         pelo_gato_negro = leer();
+         if (pelo_gato_negro != 1) {
+            console.log("error, intento numero:", k + 1);
+
+         } else {
+            console.log("excelente,nos queda uno mas");
+            k = 3;
+         }
+
+
+      }
+
+   } else {
+
+   }
+   if ((sanguijuelas_reventadas == 3) && (pulverizado_cuerno_bicornio == 5) && (pelo_gato_negro == 1)) {
+
+      console.log("ahora el ultimo ingrediente");
+
+      for (let l = 0; l < 3; l++) {
+         console.log("que cantidad de cola de serpiente?");
+         cola_serpiente = leer();
+         if (cola_serpiente != 2) {
+            console.log("maaaal, intento numero:", l + 1);
+         } else {
+
+            console.log("muy bien, completaste con exito la pocion ");
+            l = 3;
+
+         }
+
+      }
+
+   } else {
+      console.log("volve mañana  fin");
+
+   }
 }
 main();
