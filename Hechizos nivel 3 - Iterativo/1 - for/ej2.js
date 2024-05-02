bb01cf5139e09c4099b37b6211577ee26605f3c3
-1 <>
2024-05-02 10:39:38 -0300

    sd

--- Hechizos nivel 3 - Iterativo/1 - for/ej2.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej2.js
@@ -13,8 +13,8 @@ const leer = require("prompt-sync")();
  * 
  * Vida maxima del dementor: 1000
  * Vida maxima del estudiante: 350
- * Daño por turno perdido del dementor al estudiante: 75
- * Daño por hechizo acertado del estudiante contra  dementor: 267
+ * Daño por turno perdido del dementor al estudiante: 75 
+ * Daño por hechizo acertado del estudiante contra   dementor: 267
  * Turnos totales: 5   
  */
 
